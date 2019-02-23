1617424cc033fea3ad42cee9b20dba62823942a8
Jaki <vrhovecjakov@gmail.com>
2019-02-24 00:41:52 +0100

    HTML and CSS done, 3 images for paper rock and scissors, started with JS app

---
+++ app.js
@@ -0,0 +1,30 @@
+const userScore = 0;
+const compScore = 0;
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+function game() {
+
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+}
+
+
+main();
